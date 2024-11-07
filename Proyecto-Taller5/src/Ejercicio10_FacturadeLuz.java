17ab62c30c16cf48f57294ef140f7fb30b83305f
andresvallejoT-T <andresvallejo102006@gmail.com>
2024-11-07 14:29:07 -0500

    Ejercicio 12

--- Proyecto-Taller5/src/Ejercicio10_FacturadeLuz.java
+++ Proyecto-Taller5/src/Ejercicio10_FacturadeLuz.java
@@ -1,35 +1,38 @@
 
 import java.util.Scanner;
 
-/**Facturación eléctrica: Desarrollar una solución que permita calcular
- * y mostrar el valor a cancelar de una planilla de luz. Se debe ingresar 
- * el valor de costo por kilovatio/hora y el número de kilovatios consumidos
- * en el mes. Si el usuario tiene edad mayor a 65 años, se debe descontar el 
- * 10% por pertenecer a la tercera edad.
+/**
+ * Facturación eléctrica: Desarrollar una solución que permita calcular y
+ * mostrar el valor a cancelar de una planilla de luz. Se debe ingresar el valor
+ * de costo por kilovatio/hora y el número de kilovatios consumidos en el mes.
+ * Si el usuario tiene edad mayor a 65 años, se debe descontar el 10% por
+ * pertenecer a la tercera edad.
+ *
  * @author Andrés
  */
 public class Ejercicio10_FacturadeLuz {
+
     public static void main(String[] args) {
         int kilov, nvat, edad, total, desc;
         Scanner tcl = new Scanner(System.in);
-        
+
         System.out.print("Ingresar su edad: ");
         edad = tcl.nextInt();
         System.out.print("Ingresar el valor de costo por kilovatio/hora: ");
         kilov = tcl.nextInt();
         System.out.print("Ingresar el numero de kilovatios consumidos en el mes: ");
         nvat = tcl.nextInt();
-        
-        total = nvat*kilov;
-        
-        desc = (int) (-(total*0.1)+total);
-        
-        if (edad>=65){
-            System.out.println("su total es: "+desc);
-        }else{
-            System.out.println("Su total es: "+total);
+
+        total = nvat * kilov;
+
+        desc = (int) (-(total * 0.1) + total);
+
+        if (edad >= 65) {
+            System.out.println("su total es: " + desc);
+        } else {
+            System.out.println("Su total es: " + total);
         }
-        
+
     }
 }
 
@@ -40,4 +43,4 @@ Ingresar el valor de costo por kilovatio/hora: 10
 Ingresar el numero de kilovatios consumidos en el mes: 23
 su total es: 207
 BUILD SUCCESSFUL (total time: 4 seconds)
-*/ 
+ */
--- Proyecto-Taller5/src/Ejercicio12_CaculoImpuesto.java
+++ Proyecto-Taller5/src/Ejercicio12_CaculoImpuesto.java
@@ -1,12 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import java.util.Scanner;
 
 /**
+ * Calculo de impuestos: Desarrollar una solución que permita leer los datos de
+ * un automóvil (marca, origen y costo) imprima el impuesto por pagar y el
+ * precio de venta (incluido el impuesto). Si el origen es Alemania el impuesto
+ * es 20%, si es de Japón el impuesto es 30%, si es de Italia, 15%, y si es de
+ * USA, 8%
  *
  * @author Andrés
  */
 public class Ejercicio12_CaculoImpuesto {
-    
+
+    public static void main(String[] args) {
+        String marca, origen;
+        int costo;
+        Scanner tcl = new Scanner(System.in);
+
+        System.out.print("Escriba la marca del carro: ");
+        marca = tcl.next();
+        System.out.print("Escriba el pais de origen: ");
+        origen = tcl.next();
+        System.out.print("Escriba el costo del automovil: ");
+        costo = tcl.nextInt();
+
+        switch (origen) {
+            case "japon":
+                System.out.printf("la marca del automovil es: " + marca
+                        + "%nel pais de origen es: " + origen
+                        + "%nel costo total es: " + ((costo * 0.3) + costo)
+                        + "%n");
+                break;
+            case "usa":
+                System.out.printf("la marca del automovil es: " + marca
+                        + "%nel pais de origen es: " + origen
+                        + "%nel costo total es: " + ((costo * 0.08) + costo)
+                        + "%n");
+                break;
+            case "italia":
+                System.out.printf("la marca del automovil es: " + marca
+                        + "%nel pais de origen es: " + origen
+                        + "%nel costo total es: " + ((costo * 0.15) + costo)
+                        + "%n");
+                break;
+            case "alemania":
+                System.out.printf("la marca del automovil es: " + marca
+                        + "%nel pais de origen es: " + origen
+                        + "%nel costo total es: " + ((costo * 0.2) + costo)
+                        + "%n");
+                break;
+            default:
+                System.out.printf("la marca del automovil es: " + marca
+                        + "%nel pais de origen es: " + origen
+                        + "%nel costo total es: " +  costo 
+                        + "%n");
+        }
+
+    }
 }
