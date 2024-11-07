98d97abe27194e7711932a41504f44a6b8d15627
andresvallejoT-T <andresvallejo102006@gmail.com>
2024-11-07 12:31:46 -0500

    ejercicio6

---
+++ Proyecto-Taller5/src/Ejercicio6_CategoriadeEdad.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/**
+ * Categoría de Edad: Clasificar a una persona en una categoría de edad (niño,
+ * adolescente, adulto) en función de su edad.
+ *
+ * @author Andrés
+ */
+public class Ejercicio6_CategoriadeEdad {
+
+    public static void main(String[] args) {
+        int edad;
+        Scanner tcl = new Scanner(System.in);
+
+        System.out.print("Escribir su edad: ");
+        edad = tcl.nextInt();
+
+        if ((edad >= 0) && (edad <= 13)) {
+            System.out.println("usted es un niño");
+
+        } else if ((14 <= edad) && (edad <= 17)) {
+            System.out.println("Usted es adolecente");
+        } else if (edad>=18){
+            System.out.println("usted es adulto");
+        }
+
+    }
+}
